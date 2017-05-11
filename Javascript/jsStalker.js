f6d6a9ce742e420cd8304d3bbaf6aae93f0d1748
H@c_git <harishchil679@gmail.com>
2017-05-10 22:16:46 -0700

    Add files via upload

---
+++ Javascript/jsStalker.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your Firstname?");
+var lasttName = prompt("What is your Lastname?");
+var age = prompt("What is your age?");
+
+var fullName=firstName+lasttName;
+
+console.log("your full name is " + fullName );
+console.log("your age is " + age + " years old");
