fac5a0b7f72e82ea829e29ca8586a7c8167ff6f4
H@c_git <harishchil679@gmail.com>
2017-06-07 21:55:35 -0700

    forEach
    using forEach, created functions which look better in organizing the code and also is easy to read the code.

--- Javascript/list.js
+++ Javascript/list.js
@@ -7,15 +7,40 @@ while(input !== "quit"){
 
 	//handle input
 	if(input === "list"){
-		console.log(todos);
+		listTodos();
 	} else if(input === "new"){
-		//ask for the new todos
-	var newTodo = prompt("Enter new todo");
-		//add to todos array
-		todos.push(newTodo);
-}
+		addTodos();
+	}else if(input === "delete"){
+		deleteTodos();
+	};
 
 	//ask for new input
 	input = prompt("what would you like to do?");
 }
 console.log("you quit the app");
+
+function listTodos(){
+	console.log("*****************");
+		todos.forEach(function(todo,index){
+		console.log(index + ": " + todo);
+		});
+	console.log("*****************");
+};
+
+function addTodos(){
+	//ask for the new todos
+		var newTodo = prompt("Enter new todo");
+		console.log("Added todo to the list");
+			//add to todos array
+		todos.push(newTodo);
+};
+
+function deleteTodos(){
+	// ask index which needs to be deleted
+		var index=prompt("Enter the index of the todo");
+		// delete the todo
+		// splice()
+		todos.splice(index,1);
+		console.log("Deleted item in the todo list");
+
+};
